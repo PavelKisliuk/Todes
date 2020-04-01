ca58ac8d3fd4a397399b9207b09aef72f60b6f8d
Kisliuk Pavel <pavelsergeevichkisliuk2015@mail.ru>
2020-04-01 10:27:40 +0300

    Commented

--- src/main/java/com/github/pavelkisliuk/todes/entity/Cv.java
+++ src/main/java/com/github/pavelkisliuk/todes/entity/Cv.java
@@ -2,9 +2,33 @@ package com.github.pavelkisliuk.todes.entity;
 
 import java.util.Objects;
 
-public class Cv implements Todes{
+/**
+ * The {@code Cv} class is {@code Todes} realization as entity for storing
+ * another special entities which are entities in database.
+ * <p>
+ *
+ * @author Kisliuk Pavel Sergeevich
+ * @see Todes
+ * @see Person
+ * @see Contacts
+ * @see Technologies
+ * @since 13.0
+ */
+
+public class Cv implements Todes {
+	/**
+	 * Entity Person in database.
+	 */
 	private Person person;
+
+	/**
+	 * Entity Contacts in database.
+	 */
 	private Contacts contacts;
+
+	/**
+	 * Entity Technologies in database.
+	 */
 	private Technologies technologies;
 
 	public Person getPerson() {
