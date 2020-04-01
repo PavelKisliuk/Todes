c7116065bab3ccfbf8cd7ad98780dbf49d864763
Kisliuk Pavel <pavelsergeevichkisliuk2015@mail.ru>
2020-04-01 10:22:18 +0300

    Commented

--- src/main/java/com/github/pavelkisliuk/todes/entity/Todes.java
+++ src/main/java/com/github/pavelkisliuk/todes/entity/Todes.java
@@ -1,4 +1,16 @@
 package com.github.pavelkisliuk.todes.entity;
 
-public interface Todes {
-}
+/**
+ * The {@code Todes} interface is unifier for special group of classes used as
+ * entities in database.
+ * <p>
+ *
+ * @see Cv
+ * @see Person
+ * @see Contacts
+ * @see Technologies
+ * @author Kisliuk Pavel Sergeevich
+ * @since 13.0
+ */
+
+public interface Todes {}
