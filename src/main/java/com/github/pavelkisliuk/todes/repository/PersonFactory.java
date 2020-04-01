a647c85c18a6451da0acc43f32ef3a10bbe100b7
Kisliuk Pavel <pavelsergeevichkisliuk2015@mail.ru>
2020-04-01 10:50:45 +0300

    Commented

--- src/main/java/com/github/pavelkisliuk/todes/repository/PersonFactory.java
+++ src/main/java/com/github/pavelkisliuk/todes/repository/PersonFactory.java
@@ -1,12 +1,34 @@
 package com.github.pavelkisliuk.todes.repository;
 
 import com.github.pavelkisliuk.todes.entity.Person;
-import com.github.pavelkisliuk.todes.entity.Todes;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * The {@code PersonFactory} class is {@code Factory} realization for {@code Person} class
+ * creation.
+ * <p>
+ *
+ * @author Kisliuk Pavel Sergeevich
+ * @see Factory
+ * @see CvFactory
+ * @see ContactsFactory
+ * @see TechnologiesFactory
+ * @see Person
+ * @since 13.0
+ */
+
 public class PersonFactory implements Factory<Person> {
+
+	/**
+	 * Create instance of {@code Person} class used {@code ResultSet}.
+	 * <p>
+	 *
+	 * @param resultSet is {@code ResultSet} with data for class creation.
+	 * @return instance of {@code Person} class.
+	 * @throws SQLException if {@code SQLException} occurred.
+	 */
 	@Override
 	public Person create(ResultSet resultSet) throws SQLException {
 		Person person = new Person();
