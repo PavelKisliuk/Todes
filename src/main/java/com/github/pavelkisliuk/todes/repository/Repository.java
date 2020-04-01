b3e823424ace2932c490e40c4c013b0f68108629
Kisliuk Pavel <pavelsergeevichkisliuk2015@mail.ru>
2020-04-01 12:21:45 +0300

    Commented

--- src/main/java/com/github/pavelkisliuk/todes/repository/Repository.java
+++ src/main/java/com/github/pavelkisliuk/todes/repository/Repository.java
@@ -6,11 +6,40 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Repository {
-	private static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/postgres";
-	private static final String USER = "postgres";
-	private static final String PASS = "2101942882";
+/**
+ * The {@code Repository} class is repository for database.
+ * <p>
+ *
+ * @author Kisliuk Pavel Sergeevich
+ * @see Todes
+ * @see Factory
+ * @since 13.0
+ */
 
+public class Repository {
+	/**
+	 * URL to database.
+	 */
+	private static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/postgres";
+
+	/**
+	 * Username to database.
+	 */
+	private static final String USER = "";
+
+	/**
+	 * Password to database.
+	 */
+	private static final String PASS = "";
+
+	/**
+	 * Make request to database and return received data as {@code} List.
+	 * <p>
+	 *
+	 * @param request is SQL request to database.
+	 * @param factory is factory for creation expected data.
+	 * @return {@code} List of {@code Todes} elements.
+	 */
 	public List<Todes> query(String request, Factory<Todes> factory) {
 		List<Todes> list = new ArrayList<>();
 		Connection connection;
